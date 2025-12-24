d0b2854d3508258f8b2df1a6164056cc9a39b502
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2025-12-24 23:25:14 +0600

    Refactor insert and append timing into a function

--- Day_08/task_03.py
+++ Day_08/task_03.py
@@ -1,14 +1,16 @@
 import time
-
 list = [i for i in range(-50000000, 5000000)]
-start = time.time()
-list.insert(0, -5)
-end = time.time()
-time_list = end - start
-print(f"Timw taken {time_list:.6f} seconds for insert")
+def insertion_trap(position,element):
+    start = time.time()
+    list.insert(position, element)
+    end = time.time()
+    time_list = end - start
+    print(f"Timw taken {time_list:.6f} seconds for insert")
 
-start = time.time()
-list.append(-5)
-end = time.time()
-time_append = end - start
-print(f"Time taken {time_append:.6f} seconds for append")
+    start = time.time()
+    list.append(element)
+    end = time.time()
+    time_append = end - start
+    print(f"Time taken {time_append:.6f} seconds for append")
+
+insertion_trap(0, -5)
