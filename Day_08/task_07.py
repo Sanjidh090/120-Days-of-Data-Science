3cdf4cf8770d12bfd3e9cf5603f4d69d338711e1
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-06 23:41:23 +0600

    Add main guard to sort_list function

--- Day_08/task_07.py
+++ Day_08/task_07.py
@@ -1,12 +1,11 @@
 import random
+
+x = [random.randint(0, 10000) for _ in range(1000)]
 def sort_list(x = []):
-    
     import time
     start = time.time()
     x = sorted(x)
     end = time.time()
     print(f"Sorting took: {end - start} seconds")
-
-x = [random.randint(0, 10000) for _ in range(1000)]
-
-sort_list(x)
+if __name__ == "__main__": 
+    sort_list(x)
