f863e4257e3045aa41d841ca361ce91f293fbfcf
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-10 22:32:25 +0600

    Update task_02.py

--- Day_09/task_02.py
+++ Day_09/task_02.py
@@ -1,1 +1,12 @@
+import sys
+x = [ i for i in range(10**6) ]
+print(sys.getsizeof(x))
 
+def gen():
+    for i in range(10**6):
+        yield i
+print(sys.getsizeof(gen()))
+
+# 8448728
+# 104
+# The prize goes to generator
