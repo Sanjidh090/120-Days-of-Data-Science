29d832ec6764939634d5450ecd57af2d2ce62734
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-10 23:29:30 +0600

    Update task_07.py

--- Day_09/task_07.py
+++ Day_09/task_07.py
@@ -1,1 +1,6 @@
-
+def read_large_file(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            yield line
+for i in read_large_file('large_file.txt'):
+    print(i)
