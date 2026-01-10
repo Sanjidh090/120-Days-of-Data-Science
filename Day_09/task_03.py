6fcf58e1dfa2c89936586efd410287971b87564f
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-10 22:56:39 +0600

    Update task_03.py

--- Day_09/task_03.py
+++ Day_09/task_03.py
@@ -1,1 +1,10 @@
-.
+def fib_infinity(start = 0, acc = 0):
+    yield start + acc
+    yield from fib_infinity(acc, start + acc)
+
+i = fib_infinity(start = 1, acc = 0)
+for x in i:
+    print(x, end = ' ')
+    if x > 50000:
+        break
+
