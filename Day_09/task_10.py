bbfccb6c094f428ce38d58e14d8c4cce062dfedd
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-11 00:33:41 +0600

    Update task_10.py

--- Day_09/task_10.py
+++ Day_09/task_10.py
@@ -1,1 +1,23 @@
+def running_average():
+    total = 0
+    count = 0
+    avg = 0
+    while True:
+        new_value = yield avg
+        
+        if new_value is None:
+            continue
+            
+        total += new_value
+        count += 1
+        avg = total / count
+        print(f"Added {new_value}: Running Average = {avg}")
 
+c = running_average()
+next(c)      # Returns 0 (initial avg)
+print(f"Result: {c.send(10)}")  # Returns 10.0
+print(f"Result: {c.send(20)}")  # Returns 15.0
+print(f"Result: {c.send(30)}")  # Returns 20.0
+print(f"Result: {c.send(40)}")  # Returns 25.0
+print(f"Result: {c.send(50)}")  # Returns 30.0
+print(f"Result: {c.send(None)}")  # Returns 30.0 (no change)
