bc4a04e09506170e33ad5cf1c250eb0941646562
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-11 19:36:20 +0600

    Update task_03.py

--- Day_10/task_03.py
+++ Day_10/task_03.py
@@ -1,1 +1,14 @@
+def decor(func):
+    def inner(*args, **kwargs):
+        print("Before function call")
+        result = func(*args, **kwargs)
+        print("After function call")
+        return result
+    return inner
 
+@decor
+def add(a, b):
+    return a + b
+
+result = add(3, 4)
+print("Result:", result)
