351b5eabf1388ee9f3c2fd67e3ca60733381c2d7
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-14 04:49:56 +0600

    Update task_03.py

--- Day_12/task_03.py
+++ Day_12/task_03.py
@@ -1,1 +1,17 @@
+class Book:
+    def __init__(self, id, title):
+        self.id = id
+        self.title = title
+
+    def __str__(self):
+        return f"Readable Title for Users: {self.title}"
+
+    def __repr__(self):
+        return f"Book(id={self.id}, title='{self.title}')"
+
+book = Book(101, "Code")
+print(book)
+
+books = [book]
+print(books)
 
