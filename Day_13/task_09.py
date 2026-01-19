a099a7533cef32682d8534ca69f4c43aed740080
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-19 23:32:13 +0600

    Update task_09.py

--- Day_13/task_09.py
+++ Day_13/task_09.py
@@ -1,1 +1,12 @@
+import time
 
+class Timer:
+  def __enter__(self):
+    self.t = time.time()
+    time.sleep(2)
+
+  def __exit__(self, *args):
+    print(time.time() - self.t)
+
+with Timer():
+    time.time() 
